bc2b98e2bddda0dc4a648410edfe075c208d61d4
ricecutterr <robert.cretu1234@gmail.com>
2025-12-14 21:47:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -376,5 +376,6 @@ def generate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    import os
+    port = int(os.environ.get('PORT') or 8080)
     app.run(host='0.0.0.0', port=port, debug=False)
